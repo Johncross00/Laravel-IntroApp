b2bdf7725b2a673b57d9aa5dab99853d5a8e81f3
johncross00 <johncross.ktm@gmail.com>
2024-02-22 09:31:22 +0000

    topBar fixed position

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,7 @@
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.topBar')
+            @include('layouts.sideBar')
 
             <main>
                 {{-- {{ $slot }} --}}
--- resources/views/layouts/topBar.blade.php
+++ resources/views/layouts/topBar.blade.php
@@ -1,6 +1,6 @@
 <div class="app-header-inner">
     <div class="app-header-content d-flex justify-content-center align-items-center">
-        <img src="{{ asset('images/banner.jpg') }}" alt="Bannière" style="width: 100%; height: auto; max-height: 200px; border: 2px solid #fdb100; border-radius: 10px; box-shadow: 0 0 10px rgba(213, 143, 22, 0.782)">
+        <img src="{{ asset('images/banner.jpg') }}" alt="Bannière" style="width: 100%; height: auto; max-height: 200px; border: 2px solid #fdb100; border-radius: 10px; box-shadow: 0 0 10px rgba(213, 143, 22, 0.782); position: fixed;
+        ">
     </div>
 </div>
-    
