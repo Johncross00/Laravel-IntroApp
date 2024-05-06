e59474ca7d8ab9c1dfb9aac390b902bceb116c79
johncross00 <johncross.ktm@gmail.com>
2024-05-06 00:36:28 +0000

    update

--- resources/views/contents/orders.blade.php
+++ resources/views/contents/orders.blade.php
@@ -1,4 +1,19 @@
 @extends('layouts.app')
 @section('content')
 <h1><center>THIS IS THE ORDERS</center></h1>
+<form action="" method="POST">
+    @csrf
+    <label for="">Nom</label>
+    <input type="text">
+    <label for="">Prenom</label>
+    <input type="text">
+    <label for="">Age</label>
+    <input type="number">
+    <label for="">Sexe</label>
+    <label for="">M</label>
+    <input type="radio" name="s" value="F">
+    <label for="">F</label>
+    <input type="radio" name="s" value="M">
+
+</form>
 @endsection
